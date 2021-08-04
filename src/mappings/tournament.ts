16ef26ddc11bf0b2ab8a1f71a67ca53a56207885
Jeppe Qin Hansen <jeppeqin@gmail.com>
2021-08-04 16:26:43 +0200

    Entity updates

--- src/mappings/tournament.ts
+++ src/mappings/tournament.ts
@@ -1,13 +1,20 @@
 import { BigInt, dataSource } from "@graphprotocol/graph-ts"
-import { Deploy, BuyTicket } from "../../generated/templates/Tournament/tournament"
-import { Tournament, Player, TokenBalance } from "../../generated/schema"
+import { Deploy, BuyTicket, Trade, WithdrawWinnings } from "../../generated/templates/Tournament/tournament"
+import {
+  Tournament,
+  Player,
+  TokenBalance,
+  Trade as TradeEntity,
+  PlayerReward
+} from "../../generated/schema"
 
 export function handleDeploy(event: Deploy): void {
   let context = dataSource.context();
   let entity = new Tournament(context.getBytes('address').toHex())
 
-  entity.start = event.block.timestamp.plus(event.params.startBlock.minus(event.block.number).times(new BigInt(15)))
-  entity.end = event.block.timestamp.plus(event.params.endBlock.minus(event.block.number).times(new BigInt(15)))
+  entity.eventBlock = event.block.number
+  entity.start = event.block.timestamp.plus(event.params.startBlock.minus(event.block.number).times(BigInt.fromI32(15))).times(BigInt.fromI32(1000))
+  entity.end = event.block.timestamp.plus(event.params.endBlock.minus(event.block.number).times(BigInt.fromI32(15))).times(BigInt.fromI32(1000))
   entity.startBlock = event.params.startBlock
   entity.endBlock = event.params.endBlock
   entity.ticketPrice = event.params.ticketPrice
@@ -26,14 +33,42 @@ export function handleBuyTicket(event: BuyTicket): void {
   tourneyEntity.playerCount += 1
   tourneyEntity.save()
 
-  let playerEntity = new Player(player)
+  let playerEntity = new Player(tourney + player)
+  playerEntity.address = event.transaction.from
+  playerEntity.eventBlock = event.block.number
   playerEntity.tournament = tourneyEntity.id
   playerEntity.rewarded = false
   playerEntity.save()
 
-  let tokensEntity = new TokenBalance(player + tourneyEntity.ticketToken.toString())
+  let tokensEntity = new TokenBalance(event.transaction.hash.toHex())
   tokensEntity.player = playerEntity.id
   tokensEntity.token = tourneyEntity.ticketToken
   tokensEntity.amount = tourneyEntity.ticketPrice
   tokensEntity.save()
+}
+
+export function handleTrade(event: Trade): void {
+  let tourney = event.transaction.to.toHex()
+
+  let entity = new TradeEntity(event.transaction.hash.toHex())
+  entity.eventBlock = event.block.number
+  entity.player = tourney + event.params.player.toHex()
+  entity.tournament = tourney
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.fromAmount = event.params.amountFrom
+  entity.toAmount = event.params.amountTo
+  entity.time = event.block.timestamp
+  entity.save()
+}
+
+export function handleWithdrawWinnings(event: WithdrawWinnings): void {
+  let tourney = event.transaction.to.toHex()
+  let player = event.transaction.from.toHex()
+
+  let playerRewardEntity = new PlayerReward(tourney + player)
+  playerRewardEntity.eventBlock = event.block.number
+  playerRewardEntity.player = player
+  playerRewardEntity.tournament = tourney
+  playerRewardEntity.save()
 }
