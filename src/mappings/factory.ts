c67e5dccacd43b89f60d07402fb28da09c1584e9
Jeppe <jeppeqin@gmail.com>
2022-02-16 20:40:54 +0100

    update apis

--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -5,7 +5,7 @@ import { Tournament } from "../../generated/templates"
 export function handleCreateTournament(event: CreateTournament): void {
   let context = new DataSourceContext()
 
-  context.setBytes('ticketToken', event.params._ticketToken)
+  context.setBytes('ticketToken', event.params.ticketToken)
   context.setBytes('address', event.params.tournamentAddress)
   context.setString('name', event.params.name)
 
